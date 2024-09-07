6ad91879d1653b1c5715c99645847b12d82efda1
chikkaRahulgoud <goud4231@gmail.com>
2024-09-07 11:57:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require("express")
+const methodOverride = require("method-override")
+const ejsMate = require('ejs-mate');
+const app = express();
+const Listing = require('./models/listing')
+
+const mongoose = require('mongoose');
+const path = require('path')
+const port=8080
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+
+main().then(()=>{
+    console.log("connected to DB")
+})
+.catch((err)=>{
+    console.log(err)
+})
+async function main(){
+    await mongoose.connect(MONGO_URL)
+}
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+app.engine('ejs',ejsMate)
+app.use(express.static(path.join(__dirname,"/public")))
+
+app.get('/',(req,res)=>{
+    res.send("hi,I am root");
+})
+
+app.get('/listings',async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings})
+})
+
+//New route
+app.get('/listings/new',(req,res)=>{
+    res.render('listings/new.ejs')
+})
+//show route
+
+app.get('/listings/:id',async (req,res)=>{
+    let {id} = req.params;
+
+
+    const listing = await Listing.findById(id)
+    res.render('listings/show.ejs',{listing})
+})
+
+//create list
+
+app.post('/listings',async (req,res)=>{
+  const newListing =new Listing(req.body.listing)
+
+  await newListing.save();
+  res.redirect('listings')
+})
+//Edit route
+app.get('/listings/:id/edit',async (req,res)=>{
+    let {id}=req.params;
+    const listing = await Listing.findById(id)
+    res.render('listings/edit.ejs',{listing})
+})
+
+//update route
+
+app.put('/listings/:id',async (req,res)=>{
+    let {id}=req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing})
+
+   res.redirect(`/listings/${id}`)
+})
+
+//Delete route
+
+app.delete('/listings/:id',async (req,res)=>{
+    let {id}=req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id)
+    console.log(deletedListing)
+    res.redirect('/listings')
+})
+
+// app.get('/testlisting',async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title:"My New Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"calangute,Goa",
+//         country:"India"
+
+//     })
+//     await sampleListing.save()
+//     console.log("sample was saved")
+//     res.send("successful testing")
+// })
+
+
+app.listen(port,()=>{
+    console.log(`server is listening at port ${port} `);
+})
